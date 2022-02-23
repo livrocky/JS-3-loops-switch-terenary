1d420ecf33ec5a84ba37079b664f92deffabdbe4
rokas <rokasra@gmail.com>
2022-02-23 22:07:46 +0200

    cao done

---
+++ js/cao.js
@@ -0,0 +1,146 @@
+"use strict";
+
+//SWITCH SALYGOS//
+
+//1//
+
+// const car = "Bentley";
+
+// switch (car) {
+//   case "VW":
+//     console.log("VW group");
+//     break;
+//   case "Audi":
+//     console.log("VW group");
+//     break;
+//   case "Bentley":
+//     console.log("VW group");
+//     break;
+//   case "Bugatti":
+//     console.log("VW group");
+//     break;
+//   case "Porsche":
+//     console.log("VW group");
+//     break;
+//   case "BMW":
+//     console.log("BMW group");
+//     break;
+//   case "Mini":
+//     console.log("BMW group");
+//     break;
+//   case "Rolls-Royce":
+//     console.log("BMW group");
+//     break;
+// }
+
+//2//
+
+// const userInput = "obuolys";
+
+// switch (userInput) {
+//   case "bananas":
+//     console.log("vaisius");
+//     break;
+
+//   case "obuolys":
+//     console.log("vaisius");
+//     break;
+
+//   case "mangas":
+//     console.log("vaisius");
+//     break;
+
+//   case "kriause":
+//     console.log("vaisius");
+//     break;
+
+//   case "slyva":
+//     console.log("vaisius");
+//     break;
+
+//   case "bulve":
+//     console.log("darzove");
+//     break;
+
+//   case "kopustas":
+//     console.log("darzove");
+//     break;
+
+//   case "morka":
+//     console.log("darzove");
+//     break;
+
+//   case "burokas":
+//     console.log("darzove");
+//     break;
+
+//   case "pomidoras":
+//     console.log("darzove");
+//     break;
+// }
+
+//3//
+
+// const weekDay = 0;
+
+// switch (weekDay) {
+//   case 0:
+//     console.log("pirmadienis");
+//     break;
+//   case 1:
+//     console.log("antradienis");
+//     break;
+//   case 2:
+//     console.log("treciadienis");
+//     break;
+//   case 3:
+//     console.log("ketvirtadienis");
+//     break;
+//   case 4:
+//     console.log("penktadienis");
+//     break;
+//   case 5:
+//     console.log("sestadienis");
+//     break;
+//   case 6:
+//     console.log("sekamdienis");
+//     break;
+// }
+
+//TERNARY OPERATORIUS//
+
+//1//
+
+// let vardas = "Rokas";
+
+// vardas.length > 6 ? console.log("long name") : console.log("short name");
+
+//2//
+
+// let clientAge = "20";
+// let legalAge = "21";
+
+// clientAge >= 21 ? console.log("gali vairuoti") : console.log("negali vairuoti");
+
+//3//
+
+// let clientAge = "-1";
+// let legalAge = "21";
+
+// clientAge < 0 || clientAge > 120 ? console.log("invalid age") : clientAge >= 21 ? console.log("gali vairuoti") : console.log("negali vairuoti");
+
+//LOOP//
+//1-3//
+
+// let name = "Rokas";
+// let i = "i";
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i + ". " + name);
+// }
+
+//4//
+
+// for (let i = 10; i > 0; i--) {
+//   console.log(i);
+// }
--- js/switch.js
+++ js/switch.js
@@ -2,7 +2,7 @@
 // savaites dienu sk konvertavima i zodzius
 //1-7
 
-const savDn = 2;
+const savDn = 7;
 
 // if (savDn === 1) {
 //   console.log(`Pasirinkta diena: ${savDn} === Pirmadienis`);
@@ -22,32 +22,30 @@ const savDn = 2;
 //   console.log(`Pasirinkta diena: ${savDn} === Sis skaicius netinkamas dienai`);
 // }
 
-// switch (savDn) {
-//   case 1:
-//     //vykdomas kodas kai savDn === 1;
-//     console.log(`Pasirinkta diena: ${savDn} === Pirmadienis`);
-//     //break - toliau nebetikrinam ir iseinam is switch;
-//     break;
-//   case 2:
-//     console.log(`Pasirinkta diena: ${savDn} === Antradienis`);
-//     break;
-//   case 3:
-//     console.log(`Pasirinkta diena: ${savDn} === Treciadienis`);
-//     break;
-//   case 4:
-//     console.log(`Pasirinkta diena: ${savDn} === Ketvirtadienis`);
-//     break;
-//   case 5:
-//     console.log(`Pasirinkta diena: ${savDn} === Penktadienis`);
-//     break;
-//   case 6:
-//     console.log(`Pasirinkta diena: ${savDn} === Sestadienis`);
-//     break;
-//   case 7:
-//     console.log(`Pasirinkta diena: ${savDn} === Sekmadienis`);
-//     break;
-//   default:
-//     console.log(`Pasirinkta diena: ${savDn} === Sis skaicius netinkamas dienai`);
-// }
-
-//make age a switch//
+switch (savDn) {
+  case 1:
+    //vykdomas kodas kai savDn === 1;
+    console.log(`Pasirinkta diena: ${savDn} === Pirmadienis`);
+    //break - toliau nebetikrinam ir iseinam is switch;
+    break;
+  case 2:
+    console.log(`Pasirinkta diena: ${savDn} === Antradienis`);
+    break;
+  case 3:
+    console.log(`Pasirinkta diena: ${savDn} === Treciadienis`);
+    break;
+  case 4:
+    console.log(`Pasirinkta diena: ${savDn} === Ketvirtadienis`);
+    break;
+  case 5:
+    console.log(`Pasirinkta diena: ${savDn} === Penktadienis`);
+    break;
+  case 6:
+    console.log(`Pasirinkta diena: ${savDn} === Sestadienis`);
+    break;
+  case 7:
+    console.log(`Pasirinkta diena: ${savDn} === Sekmadienis`);
+    break;
+  default:
+    console.log(`Pasirinkta diena: ${savDn} === Sis skaicius netinkamas dienai`);
+}
